b06ac87c3b5c39af1f2a89b1a4f1f84ff28d0777
WinExp <winexp2333@qq.com>
2026-06-15 17:45:34 +0800

    fix: registryEnabled config is not applied on client

--- src/main/java/com/winexp/simulated_cc_compat/client/SCCCClient.java
+++ src/main/java/com/winexp/simulated_cc_compat/client/SCCCClient.java
@@ -4,20 +4,22 @@ import com.simibubi.create.foundation.item.render.SimpleCustomRenderer;
 import com.winexp.simulated_cc_compat.SCCCMod;
 import com.winexp.simulated_cc_compat.client.content.renderer.SCCCPartialModels;
 import com.winexp.simulated_cc_compat.client.content.renderer.block.PhysicsDataAccessorItemRenderer;
+import com.winexp.simulated_cc_compat.config.SCCCConfig;
 import com.winexp.simulated_cc_compat.content.registry.SCCCBlocks;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
 @Mod(value = SCCCMod.MOD_ID, dist = Dist.CLIENT)
 @OnlyIn(Dist.CLIENT)
 public class SCCCClient {
-    public SCCCClient(IEventBus modEventBus, ModContainer modContainer) {
-        SCCCPartialModels.init();
-        modEventBus.addListener(SCCCClient::registerClientExtensions);
+    public SCCCClient(IEventBus modEventBus) {
+        if (SCCCConfig.CONFIG.registryEnabled.isTrue()) {
+            SCCCPartialModels.init();
+            modEventBus.addListener(SCCCClient::registerClientExtensions);
+        }
     }
 
     public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
